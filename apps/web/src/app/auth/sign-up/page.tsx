9c5a16411eb01b633672e37dd91437571e3dcfe1
Guilherme Viana <imluizguilara@hotmail.com>
2024-06-08 16:58:07 -0300

    feat/web: setup shadcn-ui && add sign-in/sign-up screens ui

---
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignUpPage() {
+  return (
+    <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" id="email" type="email" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">Password</Label>
+        <Input name="email" id="email" type="email" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          id="password_confirmation"
+          type="password"
+        />
+      </div>
+
+      <Button type="submit" className="w-full">
+        Create account
+      </Button>
+
+      <Button variant="link" asChild className="w-full">
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" size="sm" variant="outline" className="w-full">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign up with GitHub
+      </Button>
+    </form>
+  )
+}
